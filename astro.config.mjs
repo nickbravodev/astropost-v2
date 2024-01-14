7da7630ea5d97d3f36983635a878a37bb01dabb2
Nick Bravo <nick@nickbravo.dev>
2024-01-13 19:11:45 -0700

    astro-html-beautifier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
+
+import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    mdx(),
+    htmlBeautifier({
+      indent_size: 2,
+    }),
+  ],
+});

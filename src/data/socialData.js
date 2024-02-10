66b6849e6d037d1c1dc1711a2f90afabb2c989fc
NickBravo.dev <102766832+nickbravodev@users.noreply.github.com>
2024-02-10 11:57:11 -0700

    Update socialData.js

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -1,25 +1,25 @@
 const socialData = [
-  {
-    name: "Github",
-    path: "https://www.github.com/nickbravodev/",
-    target: "_blank",
-    icon: "social/github",
-  },
+  // {
+  //   name: "Github",
+  //   path: "https://www.github.com/nickbravodev/",
+  //   target: "_blank",
+  //   icon: "social/github",
+  // },
   {
     name: "Twitter",
-    path: "https://www.twitter.com/nickbravodev/",
+    path: "https://www.twitter.com/astropost_io/",
     target: "_blank",
     icon: "social/twitter",
   },
   {
     name: "Instagram",
-    path: "https://www.instagram.com/nickbravodev/",
+    path: "https://www.instagram.com/astropost.io/",
     target: "_blank",
     icon: "social/instagram",
   },
   {
     name: "LinkedIn",
-    path: "https://www.linkedin.com/in/nickbravodev/",
+    path: "https://www.linkedin.com/company/astropost/",
     target: "_blank",
     icon: "social/linkedin",
   },

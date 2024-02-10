287512043ae0a7ecd25c8ba7204f4c0725c46757
NickBravo.dev <102766832+nickbravodev@users.noreply.github.com>
2024-02-10 11:35:55 -0700

    form updates

--- src/data/navData.js
+++ src/data/navData.js
@@ -1,39 +1,19 @@
 const navData = [
   // {
-  //   name: "About",
-  //   path: "#about",
-  // },
-  // {
-  //   name: "Scope",
-  //   path: "#scope",
-  //   // icon: {
-  //   //   name: "twitter",
-  //   //   side: "right",
-  //   // },
-  // },
-  // {
-  //   name: "Case Studies",
-  //   path: "/case-studies/",
+  //   name: "Work",
+  //   path: "/work/",
   //   // icon: {
   //   //   name: "twitter",
   //   //   side: "left",
   //   // },
   // },
-  {
-    name: "Work",
-    path: "/work/",
-    // icon: {
-    //   name: "twitter",
-    //   side: "left",
-    // },
-  },
   // {
-  //   name: "Contact",
-  //   path: "#contact",
+  //   name: "Blog",
+  //   path: "/blog/",
   // },
   {
-    name: "Blog",
-    path: "/blog/",
+    name: "Contact",
+    path: "#contact",
   },
   {
     name: "Start Your Project",
